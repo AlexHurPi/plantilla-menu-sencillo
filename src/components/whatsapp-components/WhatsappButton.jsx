d1eb97aad0d5fbf1b7138375c439ef6e7bf6875d
Alexander Hurtado <alexander.hurtado7170@gmail.com>
2026-04-06 11:29:37 -0500

    commit6

--- src/components/whatsapp-components/WhatsappButton.jsx
+++ src/components/whatsapp-components/WhatsappButton.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './whatsappButton-styles.css';
-import whatsapp from"../../../public/images/IconoWhatsapp.png";
+
 
 export const WhatsappButton = () => {
   return (
     <div className='whatsapp-container'>
-        <h3>Contactanos!</h3>
+        <h3></h3>
         <button onClick={() => window.open('https://wa.me/1234567890', '_blank')}>
-            <img src={whatsapp} alt="whatsapp" />
+            <img src="./images/IconoWhatsapp.png" alt="whatsapp" />
         </button>
     </div>
   )
